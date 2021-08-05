b75ee3251587a4c7e4869882585b1580ae9ca7c6
Tanmay Rewari <tanmayr@amdocs.com>
2021-08-05 15:16:19 +0530

    First Commit

---
+++ techademy/Employee.java
@@ -0,0 +1,7 @@
+public static void main(String args[]) {  
+class EmployeeDetails {  
+    //Creating properties of Employee class  
+int emp_id, salary;  
+String name, address, department, email;  
+  }
+}
